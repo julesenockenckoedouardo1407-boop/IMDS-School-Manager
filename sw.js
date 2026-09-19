ee9f9731014797cf49faf8d6be72574835b98ca5
julesenockenckoedouardo1407-boop <julesenockenckoedouardo1407@gmail.com>
2026-09-19 17:54:10 -0400

    Correction IMDS School Manager v2.0

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE="imds-school-manager-v2";
-const ASSETS=["./","./index.html","./manifest.json","./css/style.css","./js/app.js","./assets/icon.svg"];
+const ASSETS=["./","./index.html","./manifest.json","./style.css","./app.js","./icon.svg"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{
