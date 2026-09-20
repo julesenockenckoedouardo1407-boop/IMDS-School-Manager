ccd917452447436eb638183f0775c500ea639243
julesenockenckoedouardo1407-boop <julesenockenckoedouardo1407@gmail.com>
2026-09-20 08:12:08 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='imds-school-manager-v4-2';
+const CACHE='imds-school-manager-v4-3';
 const ASSETS=['./','./index.html','./app.js','./style.css','./manifest.json','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
